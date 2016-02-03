f9295245813ee7a1167ce897ca39fddf81bb11c3
Matthias Hertel <mathertel@hotmail.com>
2016-02-03 10:44:34 +0100

    fixing version property

--- examples/DmxSerialSend/DmxSerialSend.ino
+++ examples/DmxSerialSend/DmxSerialSend.ino
@@ -1,14 +1,20 @@
 // - - - - -
 // DmxSerial - A hardware supported interface to DMX.
-// DmxSerialSend.ino: Sample DMX application for sending 3 DMX values:
+// DmxSerialSend.ino: Sample DMX application for sending 3 DMX values.
+// There colors in the 3 lists (RedList, GreenList and BlueList) are placed into the DMX buffer with a slow fade.
+// DMXSerial works in the background and constantly sends the actual values over the DMX interface.
+// The actual values are also available on the built in PWM ports:
 // address 1 (red) -> also available on PWM Port 9
 // address 2 (green) -> also available on PWM Port 6
 // address 3 (blue) -> also available on PWM Port 5
 // 
-// Copyright (c) 2011 by Matthias Hertel, http://www.mathertel.de
+// Copyright (c) 2011-2015 by Matthias Hertel, http://www.mathertel.de
 // This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
 // 
 // Documentation and samples are available at http://www.mathertel.de/Arduino
+// The repository is on github : https://github.com/mathertel/DMXSerial
+// The repository on github is made available in the Arduino Library Manager.
+//
 // 25.07.2011 creation of the DmxSerial library.
 // 10.09.2011 fully control the serial hardware register
 //            without using the Arduino Serial (HardwareSerial) class to avoid ISR implementation conflicts.
